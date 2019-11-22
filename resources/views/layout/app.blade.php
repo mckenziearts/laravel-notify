99c5306dcc0c39db22a49bad881c81a4f070100c
Arthur Monney <monneylobe@gmail.com>
2019-11-22 08:59:57 +0100

    :lipstick: Update notify views

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -32,8 +32,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="{{ asset('vendor/mckenziearts/laravel-notify/js/app.js') }}"></script>
-    @notifyJs
     @include('notify::messages')
+    @notifyJs
 
 </body>
 </html>
--- resources/views/notify.blade.php
+++ resources/views/notify.blade.php
@@ -186,6 +186,4 @@
         </div>
     </div>
 
-    @include('notify::messages')
-
 @endsection
