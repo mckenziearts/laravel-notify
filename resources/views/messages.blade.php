40a4708cf45ba052b32484b8c418c3d78341f87f
xanxTV <29733018+XanXTV@users.noreply.github.com>
2020-08-13 23:29:26 +0100

    Revert "Update messages.blade.php - Bootstrap 4.5.X Alert Dismiss Fix"
    This reverts commit ccde2c3403bb72beb85ac20bef81f68eccef06b7.

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -1,7 +1,7 @@
 @if (session()->has('notify.message'))
 
     @if (session()->get('notify.model') === 'toast')
-        <div class="alert alert-dismissible notify-alert notify-{{ session()->get('notify.type') }} {{ config('notify.theme') }} animated {{ config('notify.animate.in_class') }}" role="alert">
+        <div class="notify-alert notify-{{ session()->get('notify.type') }} {{ config('notify.theme') }} animated {{ config('notify.animate.in_class') }}" role="alert">
             <div class="notify-alert-icon"><i class="{{ session()->get('notify.icon') }}"></i></div>
             <div class="notify-alert-text">
                 <h4>{{ session()->get('notify.title') ?? session()->get('notify.type') }}</h4>
@@ -16,7 +16,7 @@
     @endif
 
     @if (session()->get('notify.model') === 'smiley')
-        <div class="alert alert-dismissible smiley-alert smiley-{{ session()->get('notify.type') }} {{ config('notify.theme') }} animated {{ config('notify.animate.in_class') }}" role="alert">
+        <div class="smiley-alert smiley-{{ session()->get('notify.type') }} {{ config('notify.theme') }} animated {{ config('notify.animate.in_class') }}" role="alert">
             <div class="smiley-icon"><span>{{ session()->get('notify.icon') }}</span></div>
             <div class="smiley-text">
                 <p><span class="title">{{ session()->get('notify.type') }}!</span> {{ session()->get('notify.message') }}</p>
@@ -39,7 +39,7 @@
     @endif
 
     @if (session()->get('notify.model') === 'connect')
-        <div class="alert alert-dismissible connectify-alert connectify-{{ session()->get('notify.type') }} {{ config('notify.theme') }} animated {{ config('notify.animate.in_class') }}" role="alert">
+        <div class="connectify-alert connectify-{{ session()->get('notify.type') }} {{ config('notify.theme') }} animated {{ config('notify.animate.in_class') }}" role="alert">
             <div class="connectify-icon">
                 <i class="flaticon-like"></i><span>{{ session()->get('notify.type') }}</span>
             </div>
@@ -58,7 +58,7 @@
     @endif
 
     @if (session()->get('notify.model') === 'emotify')
-        <div class="alert alert-dismissible emoticon-alert emoticon-{{ session()->get('notify.type') }} animated {{ config('notify.animate.in_class') }}" role="alert">
+        <div class="emoticon-alert emoticon-{{ session()->get('notify.type') }} animated {{ config('notify.animate.in_class') }}" role="alert">
             <div class="emoticon-icon"><span></span></div>
             <div class="emoticon-text">
                 <p>{{ session()->get('notify.message') }}</p>
