1bc756fe935142ada4a39aed5f0d2b360897d01e
Arthur Monney <monneylobe@gmail.com>
2020-08-22 22:59:20 +0100

    :wrench: Update config

--- config/notify.php
+++ config/notify.php
@@ -8,13 +8,12 @@ return [
     |--------------------------------------------------------------------------
     |
     | You can change the theme of notifications by specifying the desired theme.
-    | By default the theme white is activated, but you can change it by
-    | specifying the dark mode. if null `white` theme is active. To change theme
-    | update the global variable to `dark`
+    | By default the theme light is activated, but you can change it by
+    | specifying the dark mode. To change theme, update the global variable to `dark`
     |
     */
 
-    'theme' => env('NOTIFY_THEME', null),
+    'theme' => env('NOTIFY_THEME', 'light'),
 
     /*
     |--------------------------------------------------------------------------
@@ -27,44 +26,6 @@ return [
     */
 
     'demo' => true,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Animate Module (use animate.css)
-    |--------------------------------------------------------------------------
-    |
-    | Use animate.css to animate the notice.
-    |
-    */
-
-    'animate' => [
-        'in_class'  => 'bounceInRight', // The class to use to animate the notice in.
-        'out_class' => 'bounceOutRight', // The class to use to animate the notice out.
-        'timeout'   => 5000, // Number of seconds before the notice disappears
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Notify Position
-    |--------------------------------------------------------------------------
-    |
-    | The position string option is used to describe both vertical and horizontal alignment.
-    | Element notifications and Global notifications can be vertically repositioned
-    | to: "top", "middle" or "bottom" and horizontally repositioned to: "left", "center" or "right".
-    | If we don't provide a position option the default alignments are "top right".
-    | `Don't work on drakify notification`
-    |
-    | Available positions:
-    | - "top-right"
-    | - "top-left"
-    | - "bottom-right"
-    | - "bottom-left"
-    | - "center-top"
-    | - "center-bottom"
-    |
-    */
-
-    'position' => 'top-right',
 
     /*
     |--------------------------------------------------------------------------
