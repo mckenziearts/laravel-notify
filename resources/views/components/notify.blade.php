8d1f370a63dcdfbda20705f7156a644b381583cd
Joey Geraeds <joeygeraeds@gmail.com>
2022-05-10 16:23:10 +0200

    storing data in an array and looping over them to show multiple Notify at the same time

--- resources/views/components/notify.blade.php
+++ resources/views/components/notify.blade.php
@@ -1,15 +1,24 @@
-@if (session()->has('notify.message'))
+@if (session()->has('notify.0.message'))
+    <div class="notify fixed inset-0 flex items-end justify-start px-4 py-6 pointer-events-none flex-col space-y-3 sm:p-6">
+        @php
+            $notifyList = session()->get('notify') ?: [];
+        @endphp
+        @foreach($notifyList as $key => $value)
+            @php
+                $notify = session()->pull('notify.'.$key) ?: null;
+            @endphp
+            @include('notify::notifications.toast')
 
-    @include('notify::notifications.toast')
+            @include('notify::notifications.smiley')
 
-    @include('notify::notifications.smiley')
+            @include('notify::notifications.drakify')
 
-    @include('notify::notifications.drakify')
+            @include('notify::notifications.connectify')
 
-    @include('notify::notifications.connectify')
+            @include('notify::notifications.emotify')
 
-    @include('notify::notifications.emotify')
-
+        @endforeach
+    </div>
 @endif
 
 <script>
