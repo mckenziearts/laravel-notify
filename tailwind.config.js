dead0e99d6f68752cc22f67cf4fb32463f63d9f1
Arthur Monney <monneylobe@gmail.com>
2023-03-28 06:24:59 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,7 @@
 module.exports = {
   content: ['./resources/views/**/*.blade.php'],
   theme: {
-    extend: {
-      inset: {
-        '0': 0,
-        '-14': '-3.5rem',
-      },
-      zIndex: {
-        60: '60',
-        70: '70',
-        80: '80',
-        90: '90',
-        100: '100',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
